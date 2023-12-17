a6cde9c3685a286452b7b2a2f5e176edda98869c
shunshun2021 <shunta711k2000@outlook.jp>
2023-12-17 18:48:08 +0900

    忘却曲線に基づく英単語リストの表示

--- backend/crud.py
+++ backend/crud.py
@@ -2,11 +2,42 @@ from sqlalchemy.orm import Session
 import model, schema
 import datetime
 from sqlalchemy import desc
+import math
+
+def forgetting_carve(number_of_correct_answer:int,date_of_correct_answer: datetime.datetime):
+    # 参考：https://en.wikipedia.org/wiki/Forgetting_curve
+    if(number_of_correct_answer==0):
+        return 0.0
+
+    # 忘却曲線の計算に必要な定数
+    c,k = 1.25, 1.84
+    
+    # 現在時刻の取得
+    now=datetime.datetime.now()
+    # 前回の正解時からの時間経過を算出
+    time=now-date_of_correct_answer
+    # 分単位に直す
+    time=time.seconds/60
+    # 定着率の計算
+    #b = 100*k/(pow(math.log(time),c))+k
+    b = 100*k/(pow(math.log10(time),c)+k)
+    # 1回正解するごとに記憶の減衰率が半分になるようにする。
+    forget = (100-b)/pow(2,number_of_correct_answer)
+    b = 100-forget
+    return b
+
 
 # メモ一覧取得
 def get_memos(db: Session, skip: int = 0, limit: int = 100):
-    # retentn_rateの昇順でソート
-    return db.query(model.Memo).order_by(model.Memo.retention_rate).offset(skip).limit(limit).all()
+    # 定着率(tetention_rate)の更新
+    data = db.query(model.Memo).all()
+    for sample in data:
+        sample.retention_rate = forgetting_carve(sample.number_of_correct_answer,sample.date_of_correct_answer)
+    db.commit()
+
+    # retentn_rate, date_of_correct_answer の昇順でソート
+    return db.query(model.Memo).order_by(model.Memo.retention_rate).order_by(model.Memo.date_of_correct_answer).offset(skip).limit(limit).all()
+
 
 # メモ登録
 def create_memo(db: Session, memo: schema.MemoCreatingSchema):
